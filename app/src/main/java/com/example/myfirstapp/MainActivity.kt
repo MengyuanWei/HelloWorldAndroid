352030c95170023a3e3ac3e7dac34564873e2d7f
MengyuanWei <rebec0916@gwu.edu>
2019-09-09 18:50:33 -0400

    Added comments

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // We are currently working on new_branch branch.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
